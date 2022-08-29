d0eefdfece69e202e3ada755b74b113fad130b54
dtt <t2dx.inc@gmail.com>
2022-08-29 19:04:16 +0700

    refactor constants path

--- src/main/java/com/datmt/kafka_examples/helpers/Constants.java
+++ src/main/java/com/datmt/kafka_examples/helpers/Constants.java
@@ -1,2 +1,5 @@
-package com.datmt.kafka_examples.helpers;public class Constants {
+package com.datmt.kafka_examples.helpers;
+
+public class Constants {
+    public static final String SIMPLE_TOPIC = "message_topic";
 }
